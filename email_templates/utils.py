555e33afa99d40c42f57edbaf9c982c38e170a20
Alesia Ogranovich <--replace-all>
2014-05-10 17:22:26 +0400

    core exception added

--- email_templates/utils.py
+++ email_templates/utils.py
@@ -9,18 +9,21 @@ def send_templated_email(template_code, context):
         email_template = EmailTemplate.objects.get(slug=template_code)
     except EmailTemplate.DoesNotExist:
         return
-    c = Context(context)
-    t = Template(email_template.template)
-    message_html = t.render(c)
-    message_clean = strip_tags(message_html)
-    sender = email_template.sender
-    recipients = email_template.recipients.split(',')
-    mail = EmailMultiAlternatives(email_template.subject,
-                                  message_html,
-                                  sender,
-                                  recipients,
-                                  headers={'Reply-To': sender}
-                                  )
-    if message_html != message_clean:
-        mail.attach_alternative(message_html, 'text/html')
-    mail.send(fail_silently=False)
+    try:
+        c = Context(context)
+        t = Template(email_template.template)
+        message_html = t.render(c)
+        message_clean = strip_tags(message_html)
+        sender = email_template.sender
+        recipients = email_template.recipients.split(',')
+        mail = EmailMultiAlternatives(email_template.subject,
+                                      message_html,
+                                      sender,
+                                      recipients,
+                                      headers={'Reply-To': sender}
+                                      )
+        if message_html != message_clean:
+            mail.attach_alternative(message_html, 'text/html')
+        mail.send(fail_silently=False)
+    except:
+        pass # TODO: need to handle errors
